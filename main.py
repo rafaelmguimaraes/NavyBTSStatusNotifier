570f3cf50f27606d8175a5f5f99d179c44e50c28
Rafa Guimarães <rafaelmguimaraes@gmail.com>
2023-08-03 15:47:34 -0300

    refactor: Standardize and improve naming - main.py

--- main.py
+++ main.py
@@ -1,57 +1,85 @@
+"""
+NavyBTSStatusNotifier is a Python script that fetches the status updates from
+the Capitania dos Portos da Bahia (Navy) website regarding the Bahia de Todos
+os Santos (BTS) area and sends notifications to a specified Telegram chat.
+"""
 import logging
 import logging.handlers
 import os
 from parsel import Selector
 import requests
+from enum import Enum
+
+TELEGRAM_TOKEN = None
+TELEGRAM_CHAT_ID = None
+BASE_URL = "https://www.marinha.mil.br/cpba/"
+TIMEOUT = 15
+LOG_FILE = "status.log"
 
 
-def setup_logger():
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    logger_file_handler = logging.handlers.RotatingFileHandler(
-        "status.log",
-        maxBytes=1024 * 1024,
-        backupCount=1,
-        encoding="utf8",
-    )
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    logger_file_handler.setFormatter(formatter)
-    logger.addHandler(logger_file_handler)
-    return logger
+class ErrorCode(Enum):
+    TIMEOUT = 1
+    TOKEN_NOT_AVAILABLE = 2
 
 
-def load_enviroment(logger):
+class TimeoutException(Exception):
+    pass
+
+
+class TokenNotAvailableException(Exception):
+    pass
+
+
+def load_tokens():
     try:
         TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
         TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
-        logger.info("Tokens read successfully!")
         return TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
     except KeyError:
-        logger.info("Token not available!")
-        raise
+        raise TokenNotAvailableException()
 
 
-def load_bts_status_from_navy():
+def fetch_bts_status_from_navy():
     try:
-        response = requests.get("https://www.marinha.mil.br/cpba/", timeout=15)
+        response = requests.get(BASE_URL, timeout=TIMEOUT)
+        response.raise_for_status()
         selector = Selector(text=response.text)
         statusBTS = selector.css('#block-block-17 strong::text').get()
         return statusBTS
     except requests.ReadTimeout:
-        return "Timeout (www.marinha.mil.br/cpba) - " + response.status_code
+        raise TimeoutException()
+    except requests.RequestException as e:
+        raise Exception("Error fetching BTS status: " + str(e))
 
 
-def send_message_to_telegram(token, chat_id, message, logger):
+def notify_by_telegram(token, chat_id, message):
     try:
-        url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
-        response = requests.get(url, timeout=15).json()
-        logger.info(message)
-    except requests.ReadTimeout:
-        logger.info("Timeout (api.telegram.org/bot) - " + response.status_code)
+        url = f"https://api.telegram.org/bot{token}" \
+              "/sendMessage?chat_id={chat_id}&text={message}"
+        requests.get(url, timeout=15).json()
+    except requests.RequestException as e:
+        raise Exception("Error fetching Telegram API: " + str(e))
+
+
+def main():
+    logging.basicConfig(
+        filename=LOG_FILE,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.DEBUG
+    )
+    logger = logging.getLogger(__name__)
+    try:
+        TELEGRAM_TOKEN, TELEGRAM_CHAT_ID = load_tokens()
+        statusBTS = fetch_bts_status_from_navy()
+        notify_by_telegram(TELEGRAM_TOKEN, TELEGRAM_CHAT_ID, statusBTS)
+        logger.info(statusBTS)
+    except TokenNotAvailableException:
+        logger.error("Telegram token not available. Exiting.")
+    except TimeoutException:
+        logger.error("Timeout occurred while fetching BTS status.")
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
 
 
 if __name__ == "__main__":
-    logger = setup_logger()
-    TELEGRAM_TOKEN, TELEGRAM_CHAT_ID = load_enviroment(logger)
-    statusBTS = load_bts_status_from_navy()
-    send_message_to_telegram(TELEGRAM_TOKEN, TELEGRAM_CHAT_ID, statusBTS, logger)
+    main()
